1bbfe466c59b5998f8887d5d835af18194908be2
Matthias Ludwig <matthiasludwig@me.com>
2016-07-12 01:05:54 +0000

    Added Google Map and Error Handling

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+// Gloabl Variables declared
+var map;
+var startingLoc = {lat: 37.77493, lng: -122.419416};
 
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: startingLoc,
+    zoom: 12
+  });
+}
+
+function googleError() {
+    window.alert("Google Maps could not be loaded! PANIC!");
+}
